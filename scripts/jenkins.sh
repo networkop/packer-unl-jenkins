0e10013abb07c943e8735baea84a9b456c41dd4f
networkop <mmkashin@gmail.com>
2016-02-23 22:23:43 +1100

    transfer to ubuntu

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get -y install jenkins
+service jenkins stop
+service jenkins start
